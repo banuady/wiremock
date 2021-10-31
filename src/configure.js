2bab6b403feaebbdd4fdab804b76dc32d749e5e7
banuady <banuady@yahoo.com>
2021-11-01 08:59:58 +1100

    Update configure.js

--- src/configure.js
+++ src/configure.js
@@ -3,7 +3,7 @@ const { WireMockRestClient } = require('wiremock-rest-client');
 const configureWireMock = ({ username, password, url, logLevel }) => {
   console.log(`Username: ${username}, Password: ${password}, URL: ${url}, logLevel: ${logLevel}`);
   new WireMockRestClient(`https://${username}:${password}@${url}`, {
-    logLevel: '${logLevel}',
+    logLevel: `${logLevel}`,
     continueOnFailure: false,
   });
 };
